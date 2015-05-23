65067dd09bf4a64d9abe4bdd75dd43654ef44ca6
Carsten Peterson <carsten.peterson@yale.edu>
2015-05-22 18:28:56 -0600

    templates should now be in correct format

--- templates.py
+++ templates.py
@@ -16,48 +16,73 @@ def vowels_and_consonant_clusters (word, vowels):
 		to_return.append (["c", consonant_cluster])
 	return to_return
 
-def create_templates (word, vowels, vowel_code, blank_code, dictionary, semivowel_list, nothing_special_code, blank_to_vowel_code, blank_to_consonant_code):
+def create_templates (word, vowels, vowel_code, blank_code, dictionary, semivowel_list):
 	simplified = vowels_and_consonant_clusters (word, vowels)
+
+	# semivowel_list needs to be of the form [[["-"], 0, 0], [["R"], 1, 0], ...]
+
+	# we need to keep track of what substitutions we can make, the score associated with that substitution, and, in the case of vowels, what the original vowel was
+	
+	# each entry in to_return will be of the form [["c"/"v"], [[sub1], [score1]], [[sub2], [score2]],...]
+	# the substitutions will be sorted from lowest to highest by score
+	# "c"/"v" will either be "c" indicating that that block came from a consonant (cluster), or something like "AA" indicating the vowel that that block came from
+	# lengths will keep track of the number of substitutions for each one
+
 	to_return = []
-	originals = []
 	lengths = []
+
+	# max_score is the maximum score that we are allowing:
+	# 1 for inserting vowel at the beginning
+	# 1 for inserting semivowel at beginning of consonant cluster
+	# 3 for each consonant in consonant cluster
+	# 1 for inserting semivowel at end of consonant cluster
+	# 2 for each vowel
+	# 1 for inserting vowel at the end of the word
+	max_score = 0
+
+	# vowel_subs
+	# we need semivowel list to be of the form [["", 0], ["R", 1], ...]
+
+	vowel_subs = [[[vowel_code], 0, 1], [[blank_code], 1, 0]]
+	for entry in semivowel_list:
+		vowel_subs.append ([[vowel_code, entry[0][0], vowel_code], 2+entry[1], 2])
+	length_vowel_subs = 1 + 1 + len (semivowel_list)
+
 	# if we start out with a consonant cluster, we can optionally add in a vowel at the beginning
 	if simplified[0][0] == "c":
-		to_return.append ([blank_code, vowel_code])
-		originals.append ([nothing_special_code, blank_code])
+		to_return.append ([["-"], [[[blank_code], 0], [[vowel_code], 2]]])
 		lengths.append (2)
+		max_score += 1
 	for entry in simplified:
 		# if we have a vowel, it can become 0, 1, or 2 vowels
 		if entry[0] == "v":
-			# you should create a separate code for if it's a blank to vowerl
-			to_return += [[vowel_code, blank_code], semivowel_list, [blank_code, vowel_code]]
-			originals += [[nothing_special_code, entry[1][0]], [nothing_special_code, blank_code], [blank_to_vowel_code, entry[1][0]]]
-			lengths += [2, len(semivowel_list), 2]
+			vowel = entry[1][0]
+			to_return.append ([vowel], vowel_subs)
+			lengths.append (length_vowel_subs)
+			max_score += 2
 		else:
 			# otherwise it's a consonant cluster, in which case we can insert a consonant cluster at the beginning
-			to_return += [semivowel_list]
-			lengths.append (len(semivowel_list))
-			originals.append ([nothing_special_code, blank_code])
 			of_interest = entry[1]
+			if of_interest[0] not in semivowel_set:
+				to_return += [["-"], semivowel_list] 
+				lengths += len (semivowel_list)
+				max_score += 1
 			# for each consonant, we can have C, CvC, or Cv
 			for consonant in of_interest:
-				to_return += [dictionary[consonant]]
-				lengths.append (len (dictionary[consonant]))
-				originals.append ([nothing_special_code, consonant])
-				to_return += [[blank_code, vowel_code]]
-				lengths.append (2)
-				originals.append ([nothing_special_code, blank_code])
-				to_return += [[blank_code]+dictionary[consonant]]
-				lengths.append (1+len(dictionary[consonant]))
-				originals.append ([blank_to_consonant_code, consonant])
+				# you need to create a dictionary of allowable substitutions
+				could_sub = dictionary[consonant]
+				to_return += [["-"], could_sub]
+				lengths += len (could_sub)
+				max_score += 3
 			# we can also have a semivowel at the end of the consonant cluster
-			to_return += [semivowel_list]
-			lengths.append (len (semivowel_list))
-			originals.append ([nothing_special_code, blank_code])
+			if of_interest[-1] not in semivowel_set:
+				to_return += [["-"], semivowel_list]
+				lengths += len (semivowel_list)
+				max_score += 1
 	# if we end with a consonant cluster, we can add on optional vowel
 	if simplified[-1][0] == "c":
-		to_return.append ([blank_code, vowel_code])
+		to_return.append ([["-"], [[[blank_code], 0], [[vowel_code], 2]]])
 		lengths.append (2)
-		originals.append ([nothing_special_code, blank_code])
-	return to_return, lengths, originals
+		max_score += 1
+	return to_return, lengths, max_score
 
