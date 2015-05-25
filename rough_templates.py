2de2fa5b996aedfb535efe83d69d83c296efeb0f
Carsten Peterson <carsten.peterson@yale.edu>
2015-05-25 16:40:47 -0600

    now consonant cluster templates also include rough templates

--- rough_templates.py
+++ rough_templates.py
@@ -4,15 +4,13 @@ def consonant_cluster_templates (consonant_cluster):
 	to_return = []
 	if len (consonant_cluster) == 1:
 		of_interest = consonant_cluster[0]
-		to_return += [[of_interest], [of_interest, "v", of_interest]]
+		to_return += [[["c"], [of_interest]], [["c", "v", "c"], [of_interest, "v", of_interest]]]
 	else:
 		of_interest = consonant_cluster[0]
 		all_variations = consonant_cluster_templates (consonant_cluster[1:])
 		for entry in all_variations:
-			to_return += [[of_interest] + entry, [of_interest, "v"] + entry, [of_interest, "v", of_interest] + entry]
+			to_return += [[entry[0], [of_interest] + entry[1]],[["c", "v"]+entry[0], [of_interest, "v"] + entry[1]], [["c", "v"]+entry[0], [of_interest, "v", of_interest] + entry[1]]]
 	return to_return
-
-
 
 def vowels_and_consonant_clusters (word, vowels):
 	to_return = []
